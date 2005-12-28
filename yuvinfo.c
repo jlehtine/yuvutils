d40441e8b3f9013b6c883d9e921436940c23334b
Johannes Lehtinen <johannes.lehtinen@iki.fi>
2005-12-28 06:28:35 +0000

    Added luminance and color profiling option to yuvinfo.

--- yuvinfo.c
+++ yuvinfo.c
@@ -10,6 +10,11 @@
 #define DISPLAY_ALL 0
 #define DISPLAY_LENGTH 1
 
+#define MIN_Y 16
+#define MAX_Y 235
+#define MIN_UV 16
+#define MAX_UV 240
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -17,21 +22,27 @@
 #include <unistd.h>
 #include <yuv4mpeg.h>
 
+y4m_stream_info_t stream_info;
+int plane_count;
+int plane_length[Y4M_MAX_NUM_PLANES];
+int plane_width[Y4M_MAX_NUM_PLANES];
+int plane_height[Y4M_MAX_NUM_PLANES];
+uint8_t *planes[Y4M_MAX_NUM_PLANES];
+
+static void overlay_profile(void);
+
 int main(int argc, char *argv[]) {
 	int display = DISPLAY_ALL;
 	int piping = 0;
-	y4m_stream_info_t stream_info;
+	int show_profile = 0;
 	y4m_frame_info_t frame_info;
 	int frame_length;
-	int plane_count;
-	int plane_length[Y4M_MAX_NUM_PLANES];
-	uint8_t *planes[Y4M_MAX_NUM_PLANES];
 	int use_lseek;
 	int length;
 	int i;
 	
 	/* Read options */
-	while ((i = getopt(argc, argv, "hlp")) != -1) {
+	while ((i = getopt(argc, argv, "hlpP")) != -1) {
 		switch (i) {
 			case 'h':
 				fputs(
@@ -44,13 +55,18 @@ COPYRIGHT "\n"
 "options:\n"
 "  -h     print this help text and exit\n"
 "  -l     display only the length of the stream in frames\n"
-"  -p     pipe the input to stdout and write information to stderr\n",
+"  -p     pipe the input to stdout and write information to stderr\n"
+"  -P     overlay luminance and color profile (implies -p)\n",
 					stdout);
 				exit(0);
 			case 'l':
 				display = DISPLAY_LENGTH;
 				break;
 			case 'p':
+				piping = 1;
+				break;
+			case 'P':
+				show_profile = 1;
 				piping = 1;
 				break;
 			default:
@@ -77,9 +93,27 @@ COPYRIGHT "\n"
 	}
 	for (i = 0; i < plane_count; i++) {
 		plane_length[i] = y4m_si_get_plane_length(&stream_info, i);
+		plane_width[i] = y4m_si_get_plane_width(&stream_info, i);
+		plane_height[i] = y4m_si_get_plane_height(&stream_info, i);
+		if (show_profile
+			&& (plane_width[i] * plane_height[i] != plane_length[i]
+				|| (i == 0
+					&& (plane_width[i] != y4m_si_get_width(&stream_info)
+						|| plane_height[i] != y4m_si_get_height(&stream_info))))) {
+			fputs(PROGNAME
+				": error: profiling not supported for this chroma mode\n",
+				stderr);
+			exit(1);
+		}
+	}
+	if (show_profile
+		&& (plane_width[0] < 256 || plane_height[0] < plane_count * 64)) {
+		fputs(PROGNAME ": error: frame too small for profiling overlay",
+			stderr);
+		exit(1);
 	}
 
-	/* Count the number of frames */
+	/* Process the stream */
 	y4m_init_frame_info(&frame_info);
 	length = 0;
 	if (piping || lseek(STDIN_FILENO, 0, SEEK_CUR) == -1) {
@@ -110,6 +144,9 @@ COPYRIGHT "\n"
 			if (y4m_read_frame_data(STDIN_FILENO, &stream_info, &frame_info, planes) != Y4M_OK) {
 				fputs(PROGNAME ": error: error reading frame data\n", stderr);
 				exit(1);
+			}
+			if (show_profile) {
+				overlay_profile();
 			}
 			if (piping && y4m_write_frame(STDOUT_FILENO, &stream_info, &frame_info, planes) != Y4M_OK) {
 				fputs(PROGNAME ": error error writing frame\n", stderr);
@@ -188,4 +225,62 @@ COPYRIGHT "\n"
 	}
 	
 	return 0;
+}
+
+static void overlay_profile(void) {
+	int i;
+	
+	/* Calculate and overlay value profiles for each plane */
+	for (i = 0; i < plane_count; i++) {
+		unsigned long vf[256];
+		unsigned long max;
+		double avg, var;
+		double a;
+		int x, y;
+		int j;
+		uint8_t *p;
+
+		/* Calculate value frequency */		
+		for (j = 0; j < 256; j++) {
+			vf[j] = 0;
+		}
+		p = planes[i];
+		for (j = plane_length[i]; j != 0; j--) {
+			vf[*p]++;
+			p++;
+		}
+		
+		/* Calculate maximum frequency for scaling */
+		max = 0;
+		for (j = 0; j < 256; j++) {
+			if (vf[j] > max) {
+				max = vf[j];
+			}
+		}
+		
+		/* Draw profile background */
+		y = plane_height[0] - 64 * (plane_count - i) + 4;
+		x = (plane_width[0] - 256) / 2;
+		p = planes[0] + y * plane_width[0] + x;
+		for (y = 60; y != 0; y--) {
+			for (j = 256; j != 0; j--) {
+				*p = (*p + 3 * MIN_Y) >> 2;
+				p++;
+			}
+			p += plane_width[0] - 256;
+		}
+		
+		/* Draw profile */
+		for (j = 0; j < 256; j++) {
+			int h;
+			
+			x = (plane_width[0] - 256) / 2 + j;
+			h = 60 * vf[j] / max;
+			y = plane_height[0] - 64 * (plane_count - i - 1) - h;
+			for (; h > 0; h--, y++) {
+				p = planes[0] + y * plane_width[0] + x;
+				*p = MAX_Y;
+			}
+		}
+	}
 }
